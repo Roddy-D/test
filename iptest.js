f9ff8afba7008c47b39914088d3c0bed53f5d237
Roddy <107297718+Roddy-D@users.noreply.github.com>
2026-01-30 21:30:23 -0500

    Update and rename iptest4.js to iptest.js

--- iptest.js
+++ iptest.js
@@ -4,7 +4,7 @@ const IPAPI_IS_URL = "https://api.ipapi.is/";
  
 // 从环境参数获取节点名
 const nodeName = $environment.params.node;
-const maskIP = String($environment.params?.MaskIP || "").toLowerCase() === "true";
+const maskIP = typeof $argument === "string" && $argument.includes("true");
 
 // 添加掩码函数
 function maskIpAddress(ip) {
